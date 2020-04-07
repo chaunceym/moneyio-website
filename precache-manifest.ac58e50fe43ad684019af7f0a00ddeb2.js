ed489b22089de56e771e6536df36ae85f5274ebf
mxylovezh <18322276773@163.com>
2020-04-07 20:57:13 +0800

    deploy

---
+++ precache-manifest.ac58e50fe43ad684019af7f0a00ddeb2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf59623e3bb0054dcf3d",
+    "url": "/moneyio-website/css/app.3d95c452.css"
+  },
+  {
+    "revision": "c7d75f1dadc96a8af6a3",
+    "url": "/moneyio-website/css/chunk-vendors.e11c3584.css"
+  },
+  {
+    "revision": "2f8d25a04a089bcb3ad88c7252810f4a",
+    "url": "/moneyio-website/index.html"
+  },
+  {
+    "revision": "bf59623e3bb0054dcf3d",
+    "url": "/moneyio-website/js/app.991b113c.js"
+  },
+  {
+    "revision": "c7d75f1dadc96a8af6a3",
+    "url": "/moneyio-website/js/chunk-vendors.006a31cf.js"
+  },
+  {
+    "revision": "d361f20dd880c9a210dd1f11cc3b2be6",
+    "url": "/moneyio-website/manifest.json"
+  },
+  {
+    "revision": "b3d50c6d49309be37ec8d05c3b6ccde6",
+    "url": "/moneyio-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/moneyio-website/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/moneyio-website/precache-manifest.ac58e50fe43ad684019af7f0a00ddeb2.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "savecat"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
